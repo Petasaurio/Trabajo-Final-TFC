514e43ff6bf1935e12ce3dd942d4271f6a9440f7
lolonunes <lolo.nunes2409@gmail.com>
2024-06-27 11:00:38 -0300

    Funcion graficadora.py completa.
    A las funciones polares y cartesianas solo le cambié un poco las frases de los títulos que van en las tablas y a la cartesianas le cambié un coseno por un seno que había quedado mal. A graficadora.py le agregué todos los detalles para que quede completo el gráfico, con la trayectoria de la tierra, el sol, título de los ejes y etiquetas.

--- funcion_polares.py
+++ funcion_polares.py
@@ -6,7 +6,7 @@ theta = -3/4*PI
 r = 0
 
 fichero = open("tabla_polares.dat", "w")
-fichero.write("Tabla de radios en función del ángulo:\n Ángulo [rad]\t\t Radio [UA]\n")
+fichero.write("Tabla de radios en funcion del angulo para trayectoria parabolica de perihelio {} UA:\n Angulo [rad]\t\t Radio [UA]\n".format(RP))
 
 while theta <= 3/4*PI:
     r = 2*RP/(1+m.cos(theta))
--- graficadora.py
+++ graficadora.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import math as m
 
 fichero = open("tabla_cartesianas.dat", "r")
 
@@ -7,7 +8,7 @@ lista = fichero.readlines()
 lista_x = []
 lista_y = []
 
-for i in range(1, len(lista)):
+for i in range(2, len(lista)):
     fila = lista[i].split()
 
     lista_x.append( float(fila[0]) )
@@ -15,6 +16,23 @@ for i in range(1, len(lista)):
 
 fichero.close()
 
-plt.plot(lista_x, lista_y)
+lista_xtierra=[]
+lista_ytierra=[]
+theta=0
+
+while theta <= 2*m.pi:
+    lista_xtierra.append( m.cos(theta) )
+    lista_ytierra.append( m.sin(theta) )
+    theta += 0.005
+
+plt.plot(lista_x, lista_y, label="Trayectoria del cometa C/1823 Y1")
+plt.scatter([0], [0], marker="o", color="yellow")
+plt.plot(lista_xtierra, lista_ytierra, color="red", linestyle="dashed", label="Orbita de la tierra")
+
+plt.grid()
+plt.title("Trayectoria del cometa C/1823 Y1 y la orbita de la Tierra")
+plt.xlabel( "x [UA]" )
+plt.ylabel( "y [UA]" )
+plt.legend(loc="upper left")
 plt.axis("equal")
 plt.show()
