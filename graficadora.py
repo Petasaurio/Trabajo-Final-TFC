ad590362f515cb0a44eca2ed7b76517c016ef494
gero <gero@clancy.loclahost>
2024-06-28 04:12:32 -0300

    refactorización del código de los programas parte1/graficadora.py y parte1/opcional/graficadora_opcional.py en una única función del programa graficadora.py + creación de un menú interactivo dentro del programa herschel.py que coordina la ejecución de las distintas partes del código + mejora de la graficación de las variaciones en la trayectoria [parte opcional]
    -- mejorar a futuro el estilo y la estética de las gráficas

---
+++ graficadora.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import math as m
+import coordenadas as c
+
+def graficaParte1(opcion):  # valores de opcion: 
+                            #   1 -> grafica opcional de las distintas trayectorias posibles
+                            #   0 -> grafica estandar de la trayectoria observada
+    # Se genera y dibuja la trayectoria de la Tierra primero
+
+    lista_xtierra=[]
+    lista_ytierra=[]
+    theta=0
+
+    while theta <= 2*m.pi:  #iteraciones con sentencia while para crear listas con las coordenadas de la orbita terrestre
+        lista_xtierra.append( m.cos(theta) )
+        lista_ytierra.append( m.sin(theta) )
+        theta += 0.005
+
+    plt.plot(lista_xtierra, lista_ytierra, color="red", linestyle="dashed", label="Orbita de la tierra")
+    plt.scatter([0], [0], marker="o", s=100, color="#FFC714")  
+
+    # Luego se genera la/s trayectoria/s del cometa en función de la opcion seleccionada
+
+    lista_RP=[0.22674, 0.1, 0.25, 0.5, 0.75, 1] # se trabaja con la lista de posibles valores
+
+    if opcion:
+        extremo = 6 # en caso de seleccionarse la opcion, en el [for n] el rango recorre toda la lista_RP
+        paletaColores = ["#e08295", "#e0d382", "#82c7e0", "#e0a382", "#8fe082"] # Paleta generada para dar color a las curva de la linea 49
+    else:
+        extremo = 1 # en caso contrario se usa solo el primer elemento
+
+    for n in range (0, extremo):
+        c.polares(lista_RP[n], n)
+        c.cartesianas(n)
+        fichero = open("datos/cartesiana/tablaCartesianas_{}.dat".format(n), "r")        #abrir archivo de datos para su lectura
+        lista = fichero.readlines()
+        lista_x = []
+        lista_y = []
+
+        for i in range(2, len(lista)):  #iteraciones con sentencia for para guardar la informacion del archivo en lista_x y lista_y
+            fila = lista[i].split()
+            lista_x.append( float(fila[0]) )
+            lista_y.append( float(fila[1]) )
+        fichero.close()
+
+        if not n:
+            plt.plot(lista_x, lista_y, label="Trayectoria del cometa C/1823 Y1")  
+        else:
+            plt.plot(lista_x, lista_y, linestyle ="dashed", color=paletaColores[n-1], label="Trayectoria del cometa C/1823 Y1 con RP = {}".format(lista_RP[n]))
+
+    #Configuracion de detalles para pulir la grafica
+    plt.grid()                      
+    plt.xlabel( "x [UA]" )
+    plt.ylabel( "y [UA]" )
+    plt.legend(loc="upper left")
+    plt.axis("equal")
+    
+    if opcion:
+        plt.title("Trayectoria de cometas con distinto Radio del Perihelio y la orbita de la Tierra")
+        plt.savefig("graficaOpcional.pdf", dpi=500)
+    else:
+        plt.title("Trayectoria del cometa C/1823 Y1 y la orbita de la Tierra")
+        plt.savefig("grafica.pdf", dpi=500)
+    
+    plt.show()
--- herschel.py
+++ herschel.py
@@ -1,7 +1,32 @@
 import matplotlib.pyplot as plt
 import math as m
-import coordenadas as c
+import graficadora as g
 
-RP = 0.22674
-c.polares(RP, 0)
-c.cartesianas(0)
+def menu():
+    print("¿Qué desea realizar?")
+    print("\t(0) Salir.\n")
+    print("\tParte 1)")
+    print("\t\t(a) Graficar la trayectoria del cometa junto a la órbita de la Tierra.")
+    print("\t\t    (para el valor observado por Herschel de RP)")
+    print("\t\t(b) [Opcional] Graficar las posibles trayectorias del cometa")
+    print("\t\t    junto a la órbita de la Tierra. (para los valores")
+    print("\t\t    de RP: 0,1; 0,25; 0,5; 0,75; 1 UA)\n")
+    print("\tParte 2)")
+    print("\t\t(c) Graficar la trayectoria del cometa junto a la órbita de la Tierra, con")
+    print("\t\t    las posiciones destacadas del cometa 1, 2, y 3 meses después del perihelio.")
+    print("\t\t    (para el valor observado por Herschel de RP)")
+    print("\t\t(d) [Opcional] Graficar las posibles trayectorias del cometa")
+    print("\t\t    junto a la órbita de la Tierra. (para los valores")
+    print("\t\t    de RP: 0,1; 0,25; 0,5; 0,75; 1 UA)\n")
+    return input("\t\t---> ")
+
+sel = 1 # valor arbitrario e inicial para la seleccion
+while sel != "0":
+    sel = menu()
+    if sel == "a":
+        g.graficaParte1(0)
+    elif sel == "b":
+        g.graficaParte1(1)
+    elif sel != "0":
+        print("No se reconoce la opcion!!")
+        
